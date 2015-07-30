95f01ba9d592fe8fb9920cc076527c55c6cabd4d
fede <fede.croce.123@gmail.com>
2015-07-30 09:04:48 -0300

    Actualizacion de las demas carpetas

---
+++ dev/js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".avatar-ring").click(function () {
+        $( ".avatar-ring" ).addClass( "avatar-ring-active" );
+        $( ".avatar" ).addClass( "avatar-active" );
+        $( ".nav-bar" ).addClass( "nav-bar-active" );
+        $( ".nav-bar-blur" ).addClass( "nav-bar-blur-active" );
+    });
+});
+
