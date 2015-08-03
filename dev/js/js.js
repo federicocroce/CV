ff364e16916de158956db896ddf150dfc2ab6582
Federico Croce <fede.croce.123@gmail.com>
2015-08-03 12:04:10 -0300

    Cambios en la home

--- dev/js/js.js
+++ dev/js/js.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
         $( ".avatar" ).addClass( "avatar-active" );
         $( ".nav-bar" ).addClass( "nav-bar-active" );
         $( ".nav-bar-blur" ).addClass( "nav-bar-blur-active" );
+        $( ".background-img" ).addClass( "opacity-active-background" );
+        
     });
+
+        $(".nav-bar a").click(function () {
+        	
+        	// $( ".nav-bar li" ).removeClass( "nav-bar-a" );
+        	// $(this);
+        	// $(this).parent().addClass( "nav-bar-a" );
+        // $( ".nav-bar" ).addClass( "nav-bar-active" );
+        // $( ".nav-bar-blur" ).addClass( "nav-bar-blur-active" );
+    });
+    
 });
 
