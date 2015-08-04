7d120683491de02a05a5c376d0fb11104ef3b75f
fede <fede.croce.123@gmail.com>
2015-08-04 08:50:46 -0300

    Cambios en el inicio y agregado de la home con load()

--- dev/js/js.js
+++ dev/js/js.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
     $(".avatar-ring").click(function () {
         $( ".avatar-ring" ).addClass( "avatar-ring-active" );
+        $( ".background" ).addClass( "background-active" );
+        $( ".line-background" ).addClass( "background-active" );
         $( ".avatar" ).addClass( "avatar-active" );
         $( ".nav-bar" ).addClass( "nav-bar-active" );
-        $( ".nav-bar-blur" ).addClass( "nav-bar-blur-active" );
+        $( ".line-nav-bar" ).addClass( "line-nav-bar-active" );
         $( ".background-img" ).addClass( "opacity-active-background" );
+        $( ".background-img-dark" ).addClass( "opacity-active-background" );
+        // $(".background").load('/views/home.html');
+        $(".background").load('../views/home.html');
         
     });
 
